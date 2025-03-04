bfcfff4251c7545181a7eed44948e22e65a407de
msbrett <msbrett@users.no-reply.github.com>
2025-03-03 16:29:03 -0800

    Updates

--- PS/Enable-StorageBlobLastAccessTimeTracking.ps1
+++ PS/Enable-StorageBlobLastAccessTimeTracking.ps1
@@ -2,40 +2,79 @@
 # Invoke-WebRequest -Uri "https://raw.githubusercontent.com/MSBrett/azcapman/refs/heads/main/PS/Enable-StorageBlobLastAccessTimeTracking.ps1" -OutFile "Enable-StorageBlobLastAccessTimeTracking.ps1"; ./Enable-StorageBlobLastAccessTimeTracking.ps1
 
 param(
-    [bool]$commit=$false
+    [switch]$commit
 )
+$ErrorActionPreference = "Continue"
 
-function Enable-LastAccessTimeTracking {
+function Enable-Inventory {
     param (
         [string]$rgName,
         [string]$accountName,
-        [bool]$commit
+        [string]$containerName = "inventory"
     )
     
-    Write-Output "Enabling Last Access Time Tracking for Storage Account: $accountName in Resource Group: $rgName"
-    if($commit)
-    {
-        Enable-AzStorageBlobLastAccessTimeTracking  -ResourceGroupName $rgName `
-        -StorageAccountName $accountName `
-        -PassThru `
-        -Confirm $false
-    }
-}
+        $ctx = New-AzStorageContext -StorageAccountName $accountName -UseConnectedAccount
+        $container=Get-AzStorageContainer -Context $ctx -Name $containerName
+        if($null -eq $container)
+        {
+            if($commit)
+            {   
+                $container=New-AzStorageContainer -Context $ctx -Name $containerName
+                if($null -eq $container)
+                {
+                    write-output "      Failed to create inventory container: $containerName in Storage Account: $accountName in Resource Group: $rgName"
+                }
+            }
+            else {
+                Write-Output "      Dry-run:  Will create inventory container: $containerName in Storage Account: $accountName in Resource Group: $rgName"
+            }
+        }
+        else {
+            Write-Output "      Found inventory container: $containerName in Storage Account: $accountName in Resource Group: $rgName"
+        }
+        
+        if ($null -ne $container -and $commit) {
+            $sa = Get-AzStorageAccount -ResourceGroupName $rgName -AccountName $accountName
+            if ($sa.EnableHierarchicalNamespace)
+            {
+                $rule1 = New-AzStorageBlobInventoryPolicyRule -Name blobinventory -Destination $containerName -Format Csv -Schedule Daily -BlobType blockBlob, appendBlob -IncludeSnapshot -IncludeDeleted -BlobSchemaField  Name, Creation-Time, Last-Modified, ETag, Content-Length, Content-Type, Content-Encoding, Content-Language, Content-CRC64, Content-MD5, Cache-Control, Content-Disposition, BlobType, AccessTier, AccessTierChangeTime, AccessTierInferred, LastAccessTime, LeaseStatus, LeaseState, LeaseDuration, ServerEncrypted, CustomerProvidedKeySha256, RehydratePriority, ArchiveStatus, EncryptionScope, CopyId, CopyStatus, CopySource, CopyProgress, CopyCompletionTime, CopyStatusDescription, ImmutabilityPolicyUntilDate, ImmutabilityPolicyMode, LegalHold, Deleted, RemainingRetentionDays, DeletionId,Deleted,DeletedTime,RemainingRetentionDays 
+            }
+            else {
+                $rule1 = New-AzStorageBlobInventoryPolicyRule -Name blobinventory -Destination $containerName -Format Csv -Schedule Daily -BlobType blockBlob, appendBlob -IncludeSnapshot -IncludeDeleted -BlobSchemaField  Name, Creation-Time, Last-Modified, ETag, Content-Length, Content-Type, Content-Encoding, Content-Language, Content-CRC64, Content-MD5, Cache-Control, Content-Disposition, BlobType, AccessTier, AccessTierChangeTime, AccessTierInferred, LastAccessTime, LeaseStatus, LeaseState, LeaseDuration, ServerEncrypted, CustomerProvidedKeySha256, RehydratePriority, ArchiveStatus, EncryptionScope, CopyId, CopyStatus, CopySource, CopyProgress, CopyCompletionTime, CopyStatusDescription, ImmutabilityPolicyUntilDate, ImmutabilityPolicyMode, LegalHold, Deleted, RemainingRetentionDays, Deleted, RemainingRetentionDays 
+            }
 
+            try{
+                Write-Output "      Enabling Last Access Time Tracking for Storage Account: $accountName in Resource Group: $rgName"
+                start-sleep -Seconds 1
+                $policy = Set-AzStorageBlobInventoryPolicy -StorageAccount $sa -Rule $rule1
+                Start-Sleep -Seconds 1
+                Enable-AzStorageBlobLastAccessTimeTracking  -ResourceGroupName $rgName -StorageAccountName $accountName
+            }
+            catch{
+                Write-Output "      Error enabling blob inventory for Storage Account: $accountName in Resource Group: $rgName"
+            }
+        }
+}
 
 $subscriptions = Get-AzSubscription
 foreach ($subscription in $subscriptions)
 {
-    Write-Output "Processing Subscription: $($subscription.Name)"
-    Set-AzContext -SubscriptionId $subscription.Id | Out-Null
-    $resourceGroups=Get-AzResourceGroup
-
-    foreach ($resourceGroup in $resourceGroups)
+    if ($subscription.State -ne 'Enabled')
     {
-        $storageAccounts = Get-AzStorageAccount -ResourceGroupName $resourceGroup.ResourceGroupName
-        foreach ($storageAccount in $storageAccounts)
-        {
-            Enable-LastAccessTimeTracking -rgName $resourceGroup.ResourceGroupName -accountName $storageAccount.StorageAccountName -commit $commit -Confirm $false
-        } 
+        Write-Output "Skipping $($subscription.State) subscription: $($subscription.Name)"
     }
+    else {
+        Write-Output "Processing Subscription: $($subscription.Name)"
+        Set-AzContext -SubscriptionId $subscription.Id -Tenant $subscription.TenantId | Out-Null
+        $resourceGroups=Get-AzResourceGroup
+        foreach ($resourceGroup in $resourceGroups)
+        {
+            $storageAccounts = Get-AzStorageAccount -ResourceGroupName $resourceGroup.ResourceGroupName
+            foreach ($storageAccount in $storageAccounts)
+            {
+                Write-Output "  Processing Storage Account: $($storageAccount.StorageAccountName) in Resource Group: $($resourceGroup.ResourceGroupName)"
+                Enable-Inventory -rgName $resourceGroup.ResourceGroupName -accountName $storageAccount.StorageAccountName
+            } 
+        }
+    }  
 }
