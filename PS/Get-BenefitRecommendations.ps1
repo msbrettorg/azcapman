ad55e6f220b0002e9ba1828859aa67f9d391edb8
msbrett <msbrett@users.no-reply.github.com>
2025-03-04 08:13:52 -0800

    Add term

--- PS/Get-BenefitRecommendations.ps1
+++ PS/Get-BenefitRecommendations.ps1
@@ -8,10 +8,15 @@ param (
     [Parameter()]
     [string]
     [ValidateSet('Last7Days', 'Last30Days', 'Last60Days')]
-    $LookBackPeriod='Last7Days'
+    $LookBackPeriod='Last7Days',
+
+    [Parameter()]
+    [string]
+    [ValidateSet('P1Y', 'P3Y')]
+    $term='P3Y'
 )
 
-$url="https://management.azure.com/{0}/providers/Microsoft.CostManagement/benefitRecommendations?`$filter=properties/lookBackPeriod eq '{1}' AND properties/term eq 'P1Y'&`$expand=properties/usage,properties/allRecommendationDetails&api-version=2024-08-01" -f $BillingScope, $lookBackPeriod
+$url="https://management.azure.com/{0}/providers/Microsoft.CostManagement/benefitRecommendations?`$filter=properties/lookBackPeriod eq '{1}' AND properties/term eq '{2}'&`$expand=properties/usage,properties/allRecommendationDetails&api-version=2024-08-01" -f $BillingScope, $lookBackPeriod, $term
 $uri=[uri]::new($url)
 $result = Invoke-AzRestMethod -Uri $uri.AbsoluteUri -Method GET
 $jsonResult = $result.Content | ConvertFrom-Json
